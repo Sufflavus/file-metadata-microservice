1de8cec983c53332b87a2c4e93bee61b62b774c9
Tania <tatashat@gmail.com>
2016-10-21 19:25:24 +0000

    + default page

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
+
 var app = express();
+var port = process.env.PORT || 8080;
+
+var path = process.cwd();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.sendFile(path + '/public/index.html');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/favicon.ico', function (req, res) {
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
